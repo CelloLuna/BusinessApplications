6251674a48fba09287348f63572508eaf7459136
CelloLuna <cellolunam@gmail.com>
2022-09-19 12:23:33 -0500

    end of class

--- projectOne/config/database.php
+++ projectOne/config/database.php
@@ -1,6 +1,17 @@
 <?php
-$host="";
-$dbName="";
-$userName="";
+$host="localhost";
+$dbName="cellodb";
+$userName="root";
 $password="";
+
+try {
+
+    $con = new PDO ("mysql:host={$host}; dbname={$dbName}", $userName, $password);
+    echo "Connection OK"; 
+
+} catch (PDOException $e) {
+
+	echo "Error: ".$e->getMessage();
+
+}
 ?>
